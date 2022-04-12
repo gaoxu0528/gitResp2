f27439858738785191c2276ab5f81a47c25250aa
gaoxu0528 <867276915@qq.com>
2022-04-12 22:31:45 +0800

    新增person

---
+++ TestModule01/src/com/gaoxu/Person.java
@@ -0,0 +1,11 @@
+package com.gaoxu;
+
+/**
+ * @Auther: gaoxu
+ * @Date: 2022/4/12 - 04 — 12 - 22:30
+ * @Description: com.gaoxu
+ * @Version: 1.0
+ */
+public class Person {
+    private String name;
+}
